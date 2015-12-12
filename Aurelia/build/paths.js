bc22f15beba0f1545ba1ceb13419d3a05e8b8999
Julien Enselme <julien.enselme@centrale-marseille.fr>
2015-12-12 16:27:58 +0100

    Aurelia: skeleton

---
+++ Aurelia/build/paths.js
@@ -0,0 +1,13 @@
+var appRoot = 'src/';
+var outputRoot = 'dist/';
+var exportSrvRoot = 'export/';
+
+module.exports = {
+  root: appRoot,
+  source: appRoot + '**/*.ts',
+  html: appRoot + '**/*.html',
+  css: appRoot + '**/*.css',
+  style: 'styles/**/*.css',
+  output: outputRoot,
+  exportSrv: exportSrvRoot
+};
